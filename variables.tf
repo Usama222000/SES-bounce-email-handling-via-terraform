ddb781cbe852a84e6ab66824783e64df8e0ee679
Usama Ahmed <90505506+Usama222000@users.noreply.github.com>
2023-10-04 01:59:39 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "VPC_vars" {
   type = object({
-   # AWS_REGION = string
     vpc_cidr = string
     ENV = string
     public_subnets = list(string)
@@ -9,25 +8,13 @@ variable "VPC_vars" {
   })
 }
 variable "web_sg" {
-  # type = object({
-  #   cidr = map(list(string))
-  #   ports = list(number)
-  #   name_prefix = string
-  #   max_alb_rules_limit = number
-  #   max_security_groups_limit = number
-  #   tags = object(map)
-  # })
 }
 variable "SES" {
   type = object({
     email = string
   })
 }
-# variable "SNS" {
-#   type = object({
-#     ses-arn = string
-#   })
-# }
+
 variable "Lambda" {
   type = object({
     slack_url = string
